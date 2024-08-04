f47ddda3da3691f9162dabf16a4bf0429882130c
Olha36 <olhakucheruk36@gmail.com>
2024-08-04 11:05:19 +0200

    ready: add post fuinction

--- public/script.js
+++ public/script.js
@@ -6,78 +6,107 @@ document.addEventListener('DOMContentLoaded', async function () {
   async function getPosts() {
     try {
       const response = await fetch('/db.json');
-      
+
       if (!response.ok) {
         throw new Error('Network Error Response Identified');
       }
 
       const data = await response.json();
       postsData = data.posts;
-      
-      
+
       const source = document.querySelector('#menu-template').innerHTML;
       template = Handlebars.compile(source);
-      document.querySelector('#menu-container').innerHTML = template({ posts: postsData })
+      document.querySelector('#menu-container').innerHTML = template({ posts: postsData });
     } catch (error) {
       console.error('Error fetching or processing data', error);
     }
   }
+  document.getElementById('create-post-form').addEventListener('submit', createPost);
 
   // Створення нового поста
-//   async function createPost(title, content) {
-//     try {
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+  async function createPost(title, content) {
+    title = document.getElementById('title-input').value;
+    content = document.getElementById('content-input').value;
+
+     const newPost = {
+      id: (Math.random() * 10000).toFixed(0),
+      title: title,
+      views: 0,
+      content: content,
+    };
+    try {
+      const postResponse = await fetch('/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
+      });
+
+      if (!postResponse.ok) {
+        const errorText = await postResponse.text();
+        throw new Error('Failed to add post to database', errorText);
+      }
+
+      alert('Post added successfully');
+      document.getElementById('titleInput').value = '';
+      document.getElementById('contentInput').value = '';
+
+      const updatedResponse = await fetch('/db.json');
+      if (!updatedResponse.ok) {
+        throw new Error('Failed to fetch updated data');
+      }
+      const updatedData = await updatedResponse.json();
+      postsData = updatedData.posts;
+    } catch (error) {
+      console.log('Error adding post to database', error);
+    }
+  }
 
   // Оновлення поста
-//   async function updatePost(id, title, content) {
-//     try {
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+  //   async function updatePost(id, title, content) {
+  //     try {
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
   // Видалення поста
-//   async function deletePost(id) {
-//     try {
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+  //   async function deletePost(id) {
+  //     try {
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
-//   // Додавання коментаря до поста
-//   async function createComment(postId, comment) {
-//     try {
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+  //   // Додавання коментаря до поста
+  //   async function createComment(postId, comment) {
+  //     try {
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   }
 
   // Оновлення відображення постів на сторінці
   function renderPosts(posts) {
     getPosts(posts);
-    
-
   }
 
   // Обробник події для створення поста
-//   document.getElementById('create-post-form').addEventListener('submit', cb);
+  //   document.getElementById('create-post-form').addEventListener('submit', cb);
 
-//   // Обробник події для редагування поста
-//   document.addEventListener('click', cb);
+  //   // Обробник події для редагування поста
+  //   document.addEventListener('click', cb);
 
-//   // Обробник події для видалення поста
-//   document.addEventListener('click', cb);
+  //   // Обробник події для видалення поста
+  //   document.addEventListener('click', cb);
 
-//   // Обробник події для додавання коментаря
-//   document.addEventListener('submit', cb);
+  //   // Обробник події для додавання коментаря
+  //   document.addEventListener('submit', cb);
 
   // Запуск додатку
   async function startApp() {
     renderPosts();
-    
   }
 
   startApp();
--- server.mjs
+++ server.mjs
@@ -17,10 +17,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname), 'public', 'index.html')
 });
 
-app.get('./posts', (req, res) => {
+app.get('/posts', (req, res) => {
     fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, data) => {
         if(err) {
-            console.log('Error reading database file', err);
+            console.error('Error reading database file', err);
             res.status(500).send('Error reading database file');
             return;
         }
@@ -30,6 +30,45 @@ app.get('./posts', (req, res) => {
     })
 })
 
+app.get('/posts/:id', (req, res) => {
+    const postId  = req.params.id;
+    fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, data) => {
+        if (err) {
+         console.error('Error reading database file:', err);
+         res.status(500).send('Error reading database file');
+         return;
+            
+        }
+        const db = JSON.parse(data);
+        const post = db.posts.find(p => p.id === postId);
+        if(!post) {
+            res.status(404).send('Post not found');
+            return;
+        }
+        res.status(200).send(post);
+    })
+})
+
+app.post('/posts', (req, res) => {
+    const newPost = req.body;
+    fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, data) => {
+        if(err) {
+            console.log('Error reading database file:', err);
+            res.status(500).send('Error reading database file');
+            return;
+        }
+        const db = JSON.parse(data);
+        db.posts.push(newPost);
+        fs.writeFile(path.join(__dirname, 'public', 'db.json'), JSON.stringify(db, null,  2), (err) => {
+            if(err) {
+                console.error('Error writing to database file:', err);
+                res.status(500).send('Error reading database file');
+                return;
+            }
+            res.status(201).send('Post added successfully')
+        })
+    })
+})
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     
